e00ba6e6e34a81031f509ceeb71f912be17e09d3
Alan Somers <asomers@gmail.com>
2021-06-19 17:38:18 -0600

    Delete unwanted file, for FileExt stuff

--- tokio/src/fs/file_ext.rs
+++
@@ -1,7 +0,0 @@
-pub trait FileExt {
-    pub fn write_at<'a>(&self, buf: &'a mut [u8], ofs: u64) -> WriteAt<'a, Self>
-    {
-        let std = self.std.clone();
-        asyncify(move || std.write_at(buf, offset))
-    }
-}
