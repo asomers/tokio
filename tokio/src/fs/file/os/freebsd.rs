bc19ac0a503c6f4196237dc9f9edc3313276f9f4
Alan Somers <asomers@gmail.com>
2021-06-19 17:19:31 -0600

    WIP implementing sync_all with aio on FreeBSD
    Only thing missing is the conversion from nix::Error to io::Error

--- tokio/src/fs/file/os/freebsd.rs
+++ tokio/src/fs/file/os/freebsd.rs
@@ -2,12 +2,23 @@
 // TODO: replace the generic stuff with POSIX AIO
 use super::super::super::*;
 
-#[cfg(unix)]
+use mio_aio::{AioCb, AioFsyncMode};
 use std::{
     future::Future,
+    os::unix::io::AsRawFd,
     pin::Pin,
     task::{Context, Poll},
 };
+use crate::io::PollAio;
+
+/// Represents the state of a single AIO operation
+#[derive(Debug, Eq, PartialEq)]
+enum AioState {
+    /// The AIO operation has no in-kernel state
+    Idle,
+    /// The kernel is processing the AIO operation
+    Busy
+}
 
 /// Future for the [`write_at`](crate::fs::file::FileExt::write_at) method.
 #[derive(Debug)]
@@ -30,6 +41,45 @@ impl super::super::FileExt for File {
     }
 }
 
+/// Future for the [`sync_all`](crate::fs::file::sync_all) method.
+#[derive(Debug)]
+#[must_use = "futures do nothing unless polled"]
+pub struct SyncAll {
+    state: AioState,
+    op: PollAio<AioCb<'static>>
+}
+
+impl Future for SyncAll {
+    type Output = io::Result<()>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match self.op.poll(cx) {
+            Poll::Pending => {
+                if self.state == AioState::Idle {
+                    let r = (*self.op).fsync(AioFsyncMode::O_SYNC);
+                    if let Err(e) = r {
+                        return Poll::Ready(Err(e.into()));
+                    }
+                    self.state = AioState::Busy;
+                }
+                Poll::Pending
+            },
+            Poll::Ready(Ok(_ev)) => {
+                let result = (*self.op).aio_return().map(drop);
+                Poll::Ready(result)
+            },
+            Poll::Ready(Err(e)) => Poll::Ready(Err(e))
+        }
+    }
+}
+
 pub(in super::super) async fn sync_all(file: &File) -> io::Result<()> {
-    super::generic::sync_all(file).await
+    let aiocb = AioCb::from_fd(file.std.as_raw_fd(), 0);
+    let pe = PollAio::new_for_aio(aiocb)?;
+    SyncAll{
+        state: AioState::Idle,
+        op: pe
+    }.await
+    // TODO: handle EAGAIN, EOPNOTSUPP, and EINVAL by
+    // falling back to the generic implementation.
 }
